2413b1d531de4c44813e19dfaaaa30a309835698
sldx-ws <1332644283@qq.com>
2024-04-29 00:15:40 +0800

    制作简单的静态库

---
+++ 08_dynamicAndStaticLibraries/my_add.c
@@ -0,0 +1,6 @@
+#include "my_add.h"
+
+int Add(int a, int b) {
+    printf("enter Add func, %d + %d = ?\n", a, b);
+    return a + b;
+}
---
+++ 08_dynamicAndStaticLibraries/my_sub.c
@@ -0,0 +1,6 @@
+#include "my_sub.h"
+
+int Sub(int a, int b) {
+    printf("enter Sub func: %d - %d = ?\n", a, b);
+    return a - b;
+}

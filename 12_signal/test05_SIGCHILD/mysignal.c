8d24aae620600633d1e6bfad32c1207353163127
sldx-ws <1332644283@qq.com>
2024-05-13 01:15:15 +0800

    学习SIG_CHILD信号

---
+++ 12_signal/test05_SIGCHILD/mysignal.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void Count(int cnt)
+{
+    while (cnt--)
+    {
+        printf("cnt: %2d\r", cnt);
+        fflush(stdout);
+        sleep(1);
+    }
+
+    printf("\n");
+}
+
+void handler(int signo)
+{
+    // printf("pid: %d, %d 号信号，正在被捕捉\n", getpid(), signo);
+
+    // 1、有非常多的子进程，同一时刻退出
+    // waitpid(-1) -> while(1) waitpid(-1)
+
+    // 2、有非常多的子进程，同一时刻 部分 退出
+    while (1)
+    {
+        pid_t ret = waitpid(-1, NULL, 0);
+    }
+
+}
+
+int main()
+{
+    signal(SIGCHLD, handler); // 在父进程中设置的，设置时没有子进程
+    printf("我是父进程，pid: %d, ppid: %d\n", getpid(), getppid());
+
+    pid_t id = fork();
+    if (id == 0)
+    {
+        printf("我是子进程，pid: %d, ppid: %d, 我要退出了\n", getpid(), getppid());
+        Count(5);
+        exit(1);
+    }
+
+    while (1)
+    {
+        sleep(1);
+    }
+
+    return 0;
+}
